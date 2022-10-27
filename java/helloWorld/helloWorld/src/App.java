883bdb99fadab71d7723f681bd0ec0821e566440
Louis Tsang <kawingt2@gmail.com>
2022-10-27 22:52:43 +0800

    try java hello world

---
+++ java/helloWorld/helloWorld/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        String name = "Louis";
+        String formattedGreeting = String.format("Hello, World! Welcome to Java, %s", name); 
+        System.out.println(formattedGreeting);
+    }
+}
