85bba073a21f5ae7d9c877832de61b065a3cb261
Louis Tsang <kawingt2@gmail.com>
2022-11-08 23:52:48 +0800

    learn scope in go

---
+++ go/scope/greeting.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 10, 50}
+
+func sayHello(n string) {
+	fmt.Println("Hello")
+}
+
+func showScore() {
+	fmt.Println("your score: ", score)
+}
