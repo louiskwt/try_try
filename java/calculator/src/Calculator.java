94f2844b70f9f3747fa23279f9987205fcd2559e
Louis Tsang <kawingt2@gmail.com>
2022-11-12 23:41:15 +0800

    java calculator

--- java/calculator/src/Calculator.java
+++ java/calculator/src/Calculator.java
@@ -7,18 +7,18 @@ public class Calculator {
         System.out.print("Ender the first number: ");
         double number1 = scanner.nextDouble();
         scanner.nextLine();
-
+        
         System.out.print("Enter the second number: ");
         double number2 = scanner.nextDouble();
         scanner.nextLine();
-
+        
         System.out.print("Operation to perform: ");
         String operation = scanner.nextLine();
 
         if (!operation.equals("+") && !operation.equals("-") && !operation.equals("*") && !operation.equals("/")) {
             System.out.println("Operations not supported");
         }
-
+        
         if (operation.equals("+")) {
             double output = number1 + number2;
             System.out.printf("%.1f + %.1f = %.1f \n", number1, number2, output);
@@ -32,8 +32,8 @@ public class Calculator {
          if (operation.equals("*")) {
             double output = number1 * number2;
             System.out.printf("%.1f * %.1f = %.1f \n", number1, number2, output);
-         }
-         
+        }
+        
          if (operation.equals("/")) {
             if (number2 == 0) {
                 System.out.println("Cannot divide by 0");
@@ -42,7 +42,6 @@ public class Calculator {
                 System.out.printf("%.1f * %.1f = %.1f \n", number1, number2, output);
             }
          }
-
-         scanner.close();
+      scanner.close();
     }
 }
