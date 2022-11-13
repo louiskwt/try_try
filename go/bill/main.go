a036c52c752de740257abd8416c75ec2665c8f05
Louis Tsang <kawingt2@gmail.com>
2022-11-13 12:06:12 +0800

    switch statement and parse float

--- go/bill/main.go
+++ go/bill/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -30,12 +31,48 @@ func promptOptions(b bill) {
 
 	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
 
-	fmt.Println(opt)
+	switch opt {
+		case "a":
+			name, _ := getInput("Item name: ", reader)
+			price, _ := getInput("Item price: ", reader)
+
+			p, err := strconv.ParseFloat(price, 64)
+
+			if err != nil {
+				fmt.Println("The price must be a number")
+				promptOptions(b)
+			}
+
+			b.addItem(name, p)
+
+			fmt.Println("Item added - ", name, p)
+			promptOptions(b)
+		case "t":
+			tip, _ := getInput("Enter tip amount ($): ", reader)
+
+			t, err := strconv.ParseFloat(tip, 64)
+
+			if err != nil {
+				fmt.Println("The price must be a number")
+				promptOptions(b)
+			}
+
+			b.updateTip(t)
+
+			fmt.Println("Tip updated - ", t)
+			promptOptions(b)
+
+		case "s":
+			fmt.Println("Saved bill - ", b.format())
+		default:
+			fmt.Println("That was not a valid option...")
+			promptOptions(b)
+	}
 
 }
 
 func main() {
 	myBill := createBill()
 	promptOptions(myBill)
-	fmt.Println(myBill)
+
 }
