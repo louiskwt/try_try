f147c3e0d73021127f29eebfa4e185a1870779b5
Louis Tsang <kawingt2@gmail.com>
2022-11-22 23:26:54 +0800

    wrote and pushed a simple lambda function

--- react-amplify/amplify/backend/function/customer/src/index.js
+++ react-amplify/amplify/backend/function/customer/src/index.js
@@ -1,17 +1,18 @@
-
-
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+  console.log(event);
+  const customerId = event.pathParameters.customerId; // customers/id
+  const customer = {
+    customerId: customerId,
+    customerName: "Customer" + customerId,
+  };
+  const response = {
+    statusCode: 200,
+    // Uncomment the following to enable CORS requests
+    headers: {
+      "Access-control-Allow-Origin": "*",
+      "Access-control-Allow-Headers": "*",
+    },
+    body: JSON.stringify(customer),
+  };
+  return response;
 };
--- react-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ react-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "customer": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "amplifyReact": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
