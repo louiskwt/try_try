32c5c9e0323a4ff7029bfaf1f9a2b39bacda655f
Louis Tsang <kawingt2@gmail.com>
2022-11-23 07:50:21 +0800

    connect react app to amplify

--- react-amplify/src/App.js
+++ react-amplify/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { API } from "aws-amplify";
+import React, { useState } from "react";
 
-function App() {
+const myAPI = "amplifyReact";
+const path = "/customers";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [customers, setCustomers] = useState([]);
+
+  // Function to fetch from our backend and update customer array
+  function getCustomer(e) {
+    let customerId = e.input;
+    API.get(myAPI, `${path}/${customerId}`)
+      .then((response) => {
+        console.log(response);
+        let newCustomers = [...customers];
+        newCustomers.push(response);
+        setCustomers(newCustomers);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple React App</h1>
+      <div>
+        <input
+          placeholder="Customer Id..."
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <br />
+      <button
+        onClick={() => {
+          getCustomer({ input });
+          setInput("");
+        }}
+      >
+        Get Customer
+      </button>
+      <br />
+
+      <h2 style={{ visibility: customers.length > 0 ? "visible" : "hidden" }}>
+        Response
+      </h2>
+      {customers.map((customer, index) => {
+        return (
+          <div key={index}>
+            <span>
+              <b>Customer Id: </b> {customer.customerId} -{" "}
+              <b>{customer.customerName}</b>
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
