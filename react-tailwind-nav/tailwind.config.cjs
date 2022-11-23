29499f037a3c592c95707158e6e88d771119bc55
Louis Tsang <kawingt2@gmail.com>
2022-11-24 07:46:48 +0800

    reconfigure tailwind

--- react-tailwind-nav/tailwind.config.cjs
+++ react-tailwind-nav/tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ["./src/**/*.html", "./index.html"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gray: colors.neutral,
+      gray: {
+        900: "#202225",
+        800: "#2f3136",
+        700: "#36393f",
+        600: "#4f545c",
+        400: "#d4d7dc",
+        300: "#e3e5e8",
+        200: "#ebedef",
+        100: "#f2f3f5",
+      },
+    },
   },
   plugins: [],
 };
