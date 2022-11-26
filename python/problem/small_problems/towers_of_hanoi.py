21dcf2436f198804fe2111db8031e41d9f0ab3a8
Louis Tsang <kawingt2@gmail.com>
2022-11-26 11:52:41 +0800

    tower of hanoi iterative

--- python/problem/small_problems/towers_of_hanoi.py
+++ python/problem/small_problems/towers_of_hanoi.py
@@ -14,11 +14,14 @@ class Stack(Generic[T]):
     def pop(self) -> T:
         return self._container.pop()
     
+    def isEmpty(self) -> bool:
+        return not self._container
+    
     def __repr__(self) -> str:   # specifiy what will be output when print() is applied to a Stack.
         return repr(self._container)
 
 
-num_discs: int = 3
+num_discs: int = 144
 tower_a: Stack[int] = Stack()
 tower_b: Stack[int] = Stack()
 tower_c: Stack[int] = Stack()
@@ -37,8 +40,28 @@ def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
         hanoi(begin, end, temp, 1)
         hanoi(temp, end, begin, n - 1)
 
+# total moves = 2^n - 1
+total_moves = pow(2, num_discs) - 1
+
+print(total_moves)
+
+def hanoi2(begin: Stack[int], end: Stack[int], temp: Stack[int], total: int) -> None:
+    for i in range(1, total):
+        if not begin.isEmpty() and i % 3 == 1:
+            end.push(begin.pop())
+        
+        if not begin.isEmpty() and i % 3 == 2:
+            temp.push(begin.pop())
+        
+        if not temp.isEmpty() and i % 3 == 0:
+            end.push(temp.pop())
+
+        if temp.isEmpty() and begin.isEmpty():
+            break
+
+
 if __name__ == "__main__":
-    hanoi(tower_a, tower_c, tower_b, num_discs)
+    hanoi2(tower_a, tower_c, tower_b, total_moves)
 
     print(tower_a)
     print(tower_b)
