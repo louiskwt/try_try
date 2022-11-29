029fbe724110ba9080e5f4f3f3673bb4c43508e6
Louis Tsang <kawingt2@gmail.com>
2022-11-30 07:35:26 +0800

    learning java

---
+++ java/tutorial/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Input your age");
+        Scanner SC = new Scanner(System.in);
+        String s = SC.nextLine();
+
+        int age = Integer.parseInt(s);
+
+        if (age >= 13) {
+            System.out.println("You can ride");
+        } 
+        else {
+            System.out.println("You cannot ride");
+        }
+
+        
+    }
+}
