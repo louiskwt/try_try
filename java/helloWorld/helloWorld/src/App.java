1ebe2599a2241fcf48301032475f0cfe56fac8ad
Louis Tsang <kawingt2@gmail.com>
2022-11-30 08:00:15 +0800

    cleaned up

--- java/helloWorld/helloWorld/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        String name = "Louis";
-        String formattedGreeting = String.format("Hello, World! Welcome to Java, %s", name); 
-        System.out.println(formattedGreeting);
-    }
-}
