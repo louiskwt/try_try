c02d1e227e59f3a221ca894785a2ab7c10947c46
Louis Tsang <kawingt2@gmail.com>
2023-03-06 10:21:07 +0800

    improve selection sort

--- python/algorithms/Ch2_array_linklist_selection_sort/selection_sort.py
+++ python/algorithms/Ch2_array_linklist_selection_sort/selection_sort.py
@@ -9,15 +9,27 @@ def findSmallest(arr):
     
     return smallest_index
 
+def findLargest(arr):
+    largest = arr[0]
+    largest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] > largest:
+            largest = arr[i]
+            largest_index = i
+    return largest_index
+
+
 # Big O = n^2
 
-def selectionSort(arr):
+def selectionSort(arr, order):
     newArr = []
+    sorting_func = findSmallest if order == "asc" else findLargest
     for _ in range(len(arr)):
-        smallest = findSmallest(arr)
+        smallest = sorting_func(arr)
         newArr.append(arr.pop(smallest))
     
     return newArr
 
 
-print(selectionSort([13, 27, 903, 100, 2304, 234, 65, 2, 4, 45]))
+print(selectionSort([13, 27, 903, 100, 2304, 234, 65, 2, 4, 45], "desc"))
+print(selectionSort([13, 300000, 342, 27, 903, 100, 2304, 234, 65, 2, 4, 45, 1], "asc"))
