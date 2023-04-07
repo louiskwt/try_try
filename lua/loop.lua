0ff6829c733c625c0d0fc9c0e7a65833be93aa2f
Louis Tsang <kawingt2@gmail.com>
2023-04-07 10:54:38 +0800

    table and loop

---
+++ lua/loop.lua
@@ -0,0 +1,23 @@
+tbl1 = {}
+tbl1["doggy"] = "woof"
+key = "duck"
+tbl1[key] = "quack"
+
+for k, v in pairs(tbl1) do
+    print(k .. " say " .. v)
+end
+
+local tbl2 = {"item 1", "item 2", "item 3"}
+
+for k, v in pairs(tbl2) do
+    -- index start with 1
+    print(k)
+end
+
+-- empty table is not nil
+tbl3 = {}
+
+print(type (tbl3) == "nil" )
+
+print(type (x) == nil) -- false
+print(type (x) == "nil") -- true
