e894d3435ee371851e943838e43f9d5680892e66
Louis Tsang <kawingt2@gmail.com>
2023-04-08 21:46:27 +0800

    revised variables

--- lua/hello.lua
+++ lua/hello.lua
@@ -10,3 +10,22 @@ print(str)
 -- [[
 --lol
 --]]
+
+a = 5
+local b = 28
+
+function number()
+    c = a * 5
+    local d = 5 * 2
+    print(c)
+end
+
+number()
+print(b)
+print(c, d)
+
+do
+    local a = 6
+    b = 30
+    print(a, b)
+end
