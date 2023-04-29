dbc82dac528b3cbd9f8d1daa1e10b409afdebbf6
Louis Tsang <kawingt2@gmail.com>
2023-04-29 22:48:58 +0800

    used hoisting in C

--- c/function/function.c
+++ c/function/function.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-
-int mymult(int a, int b) {
-    int c = a * b;
-    return c;
-}
-
 int main() {
     int mymult();
     int retval;
 
     retval = mymult(6,7);
     printf("Answer: %d\n",retval);
+}
+
+int mymult(int a, int b) {
+    int c = a * b;
+    return c;
 }
