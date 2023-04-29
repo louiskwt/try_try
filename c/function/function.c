233b8e9da7b54798010faec6af633717a9b8f189
Louis Tsang <kawingt2@gmail.com>
2023-04-29 22:48:04 +0800

    wrote function in C

---
+++ c/function/function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int mymult(int a, int b) {
+    int c = a * b;
+    return c;
+}
+
+int main() {
+    int mymult();
+    int retval;
+
+    retval = mymult(6,7);
+    printf("Answer: %d\n",retval);
+}
