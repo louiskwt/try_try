86d1d254b3d00a8f4dd5da1104fcd3ce884f0b67
Louis Tsang <kawingt2@gmail.com>
2023-05-12 08:34:57 +0800

    mario c wip

---
+++ c/mario/mario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void draw(int h);
+
+int main()
+{
+    int height = 3;
+    draw(height);
+}
+
+void draw(int h)
+{
+    // base case goes first
+   
+    for (int i = 0; i < h; i++) 
+    {
+        for (int j = 0; j < h - i; j++) 
+        {
+            printf(" ");
+        }
+
+        printf("#");
+    }
+    
+    printf("\n");
+}
