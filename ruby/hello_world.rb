7ea215a5b908da458770d22798e0864d5ef333ce
Louis Tsang <kawingt2@gmail.com>
2023-06-06 23:49:34 +0800

    learned ruby func and hash

--- ruby/hello_world.rb
+++ ruby/hello_world.rb
@@ -6,3 +6,14 @@ def say_hello(name)
 end
 
 say_hello(name)
+
+over18 = "Ometedo! You're an adult"
+under18 = "Oh, you're too young, "
+user = {name: "Moa", from: "Metalverse"}
+
+if age >= 18
+  puts "Hi, #{user[:name]} #{over18}"
+else
+  puts "#{under18} #{user[:name]}"
+end
+
