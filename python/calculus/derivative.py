25c21711c9aea48a48ac2147c98d8953df26cc78
Louis Tsang <kawingt2@gmail.com>
2023-09-22 23:25:59 +0800

    pushed calculus py

--- python/calculus/derivative.py
+++ python/calculus/derivative.py
@@ -1,28 +1,37 @@
 from sympy import *
 
-x, y, z = symbols('x y z')
+# x, y, z = symbols('x y z')
 
-f = 2*x**2+5
+# f = 2*x**2+5
 
-g = 3*sin(y)
+# g = 3*sin(y)
 
-h = sin(x)/cos(x)
+# h = sin(x)/cos(x)
 
-df = diff(f, x)
+# df = diff(f, x)
 
-print(df)
+# print(df)
 
-dg = diff(g, y)
+# dg = diff(g, y)
 
-print(dg)
+# print(dg)
 
-dh = diff(h, x)
-print(dh)
+# dh = diff(h, x)
+# print(dh)
 
-i = symbols('i')
+# i = symbols('i')
 
-u = 3*i**4 + 2*i**3 - i ** 2 + 36
+# u = 3*i**4 + 2*i**3 - i ** 2 + 36
 
-d2u = diff(u, i, 3)
+# d2u = diff(u, i, 3)
 
-print(d2u)
+# print(d2u)
+
+# complex derivative
+j = symbols('j')
+
+w = (1 / (j ** 2 + 4))
+
+d2w = diff(w, j, 2)
+
+print(d2w)
