958ccb09aa80fec2a5fd87245b44317f09c72d94
Louis Tsang <kawingt2@gmail.com>
2024-11-13 17:36:57 +0800

    trying fastify

---
+++ fastify-playground/start.cjs
@@ -0,0 +1,12 @@
+const fastify = require("fastify");
+const serverOptions = {logger: true};
+
+const app = fastify(serverOptions);
+app
+  .listen({
+    port: 8000,
+    host: "0.0.0.0",
+  })
+  .then((address) => {
+    console.log(`Server listening on ${address}`);
+  });
