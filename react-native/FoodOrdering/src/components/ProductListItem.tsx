dc2d56ab1a597ccd7f8c8d25a0e152d7f2da448b
Louis Tsang <kawingt2@gmail.com>
2024-11-27 13:36:21 +0800

    flat list and menu screen

--- react-native/FoodOrdering/src/components/ProductListItem.tsx
+++ react-native/FoodOrdering/src/components/ProductListItem.tsx
@@ -11,7 +11,7 @@ type ProductListItemProps = {
 const ProductListItem = ({product}: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} />
+      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} resizeMode="contain" />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -22,9 +22,12 @@ export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   title: {
     fontSize: 18,
