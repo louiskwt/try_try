ad6e9ca7c1228941b61054d1aef4a1632ca9ed28
Louis Tsang <kawingt2@gmail.com>
2024-11-28 11:33:57 +0800

    added product image, size selection and price to detail screen

--- react-native/FoodOrdering/src/app/(tabs)/menu/[id].tsx
+++ react-native/FoodOrdering/src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,73 @@
+import {defaultPizzaImage} from "@/components/ProductListItem";
+import products from "@assets/data/products";
 import {Stack, useLocalSearchParams} from "expo-router";
-import {Text, View} from "react-native";
+import {useState} from "react";
+import {Image, Pressable, StyleSheet, Text, View} from "react-native";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
   const {id} = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState("M");
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) return <Text>Product not found</Text>;
+
   return (
-    <View>
-      <Stack.Screen options={{title: "Detail"}} />
-      <Text style={{fontSize: 20}}>Porduct Details for id {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{title: product?.name || "Pizza"}} />
+      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} />
+
+      <Text>Select Size</Text>
+
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            key={size}
+            onPress={() => {
+              setSelectedSize(size);
+            }}
+            style={[styles.size, {backgroundColor: selectedSize === size ? "gainsboro" : "white"}]}>
+            <Text style={[styles.sizeText, {color: selectedSize === size ? "black" : "gray"}]}>{size}</Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 30,
+    fontWeight: 500,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
 export default ProductDetailScreen;
