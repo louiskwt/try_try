36a2de39a24c6d531a758909ace28d38781bc5d2
Louis Tsang <kawingt2@gmail.com>
2024-11-28 16:10:48 +0800

    set up product create form

--- react-native/FoodOrdering/src/app/(admin)/menu/_layout.tsx
+++ react-native/FoodOrdering/src/app/(admin)/menu/_layout.tsx
@@ -11,7 +11,7 @@ export default function MenuStack() {
         options={{
           title: "Menu",
           headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>{({pressed}) => <FontAwesome name="plus-square-o" size={20} color={Colors.light.tint} style={{marginRight: 15, opacity: pressed ? 0.5 : 1}} />}</Pressable>
             </Link>
           ),
