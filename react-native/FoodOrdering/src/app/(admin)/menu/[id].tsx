4f6286f95acdb5bdb40dd26576c621a8d51ca4a2
Louis Tsang <kawingt2@gmail.com>
2024-11-29 10:19:37 +0800

    removed code from admin

--- react-native/FoodOrdering/src/app/(admin)/menu/[id].tsx
+++ react-native/FoodOrdering/src/app/(admin)/menu/[id].tsx
@@ -1,27 +1,16 @@
 import {defaultPizzaImage} from "@/components/ProductListItem";
 import Colors from "@/constants/Colors";
-import {useCart} from "@/providers/CartProvider";
 import {PizzaSize} from "@/types";
 import products from "@assets/data/products";
 import {FontAwesome} from "@expo/vector-icons";
-import {Link, Stack, useLocalSearchParams, useRouter} from "expo-router";
-import {useState} from "react";
+import {Link, Stack, useLocalSearchParams} from "expo-router";
 import {Image, Pressable, StyleSheet, Text, View} from "react-native";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
   const {id} = useLocalSearchParams();
-  const {addItem} = useCart();
-  const router = useRouter();
-
-  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
   const product = products.find((p) => p.id.toString() === id);
-  const addToCart = () => {
-    if (!product) return;
-    addItem(product, selectedSize);
-    router.push("/cart");
-  };
 
   if (!product) return <Text>Product not found</Text>;
 
