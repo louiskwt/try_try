05155b17f4d05c1e32ec41e392d5c17e03a9fd69
Louis Tsang <kawingt2@gmail.com>
2024-11-29 16:03:27 +0800

    finished top tap navigator

--- react-native/FoodOrdering/src/app/(admin)/orders/_layout.tsx
+++ react-native/FoodOrdering/src/app/(admin)/orders/_layout.tsx
@@ -3,7 +3,7 @@ import {Stack} from "expo-router";
 export default function OrderStack() {
   return (
     <Stack screenOptions={{}}>
-      <Stack.Screen name="index" options={{title: "Orders"}} />
+      <Stack.Screen name="list" options={{headerShown: false, title: "Orders"}} />
     </Stack>
   );
 }
