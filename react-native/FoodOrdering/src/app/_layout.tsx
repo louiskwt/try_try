c3b11bc755732db840cc19591e4e624fea70679f
Louis Tsang <kawingt2@gmail.com>
2024-11-30 23:34:34 +0800

    set up auth prrovider

--- react-native/FoodOrdering/src/app/_layout.tsx
+++ react-native/FoodOrdering/src/app/_layout.tsx
@@ -8,6 +8,7 @@ import "react-native-reanimated";
 
 import CartProvider from "@/providers/CartProvider";
 
+import AuthProvider from "@/providers/AuthProvider";
 import {useColorScheme} from "@components/useColorScheme";
 
 export {
@@ -52,14 +53,16 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <CartProvider>
-        <Stack>
-          <Stack.Screen name="(admin)" options={{headerShown: false}} />
-          <Stack.Screen name="(auth)" options={{headerShown: false}} />
-          <Stack.Screen name="(user)" options={{headerShown: false}} />
-          <Stack.Screen name="cart" options={{presentation: "modal"}} />
-        </Stack>
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <Stack>
+            <Stack.Screen name="(admin)" options={{headerShown: false}} />
+            <Stack.Screen name="(auth)" options={{headerShown: false}} />
+            <Stack.Screen name="(user)" options={{headerShown: false}} />
+            <Stack.Screen name="cart" options={{presentation: "modal"}} />
+          </Stack>
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
