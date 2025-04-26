89b08ded751045549c59e7318db4ed268790f1e5
tkw <kawint2@gmail.com>
2025-04-26 02:40:27 -0700

    clean up

--- shadcn/src/App.tsx
+++ shadcn/src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
 import { Button } from './components/ui/button'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
