7e3d06d2f92a027366edd098086f8f3ac1af2fef
Louis Tsang <kawingt2@gmail.com>
2025-04-27 23:11:24 +0800

    played with icon

--- shadcn/src/App.tsx
+++ shadcn/src/App.tsx
@@ -1,8 +1,12 @@
 import {Button} from "@/components/ui/button";
+import {CirclePlus} from "lucide-react";
 function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <Button variant="destructive">Click me</Button>
+      <Button variant="destructive" size={"lg"}>
+        <CirclePlus />
+        Click me
+      </Button>
     </div>
   );
 }
